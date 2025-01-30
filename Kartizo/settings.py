306e6a2f35d4fb472fe364ddc829881aa7e25a72
Anie22 <francisaniebiet678@gmail.com>
2025-01-30 16:26:15 +0100

    first commit

--- Kartizo/settings.py
+++ Kartizo/settings.py
@@ -186,9 +186,9 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtppro.zoho.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'Kartizo <no-reply@kartizoglobal.com>'
+DEFAULT_FROM_EMAIL = 'Kartizo <francisaniebiet223@gmail.com>'
