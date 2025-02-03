3f5c555ffea7b634f295e98e26c30d1de9d80c54
Aniebiet Francis <102663124+Anie22@users.noreply.github.com>
2025-02-03 15:11:41 -0800

    update

--- form/views.py
+++ form/views.py
@@ -26,5 +26,4 @@ class CreateContactView(GenericAPIView):
                 print(str(e))
                 return Response({"message": "Email sending failed", "err": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             return Response({'message':'Your order request has been made'}, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
