712969cb84efa4d0d588aad56dfb326227bca2ca
Aniebiet Francis <102663124+Anie22@users.noreply.github.com>
2025-02-03 15:06:05 -0800

    update

--- form/serializers.py
+++ form/serializers.py
@@ -1,21 +1,21 @@
 from rest_framework import serializers
-from django.utils.translation import gettext_lazy as _
 from phonenumber_field.serializerfields import PhoneNumberField
-from form.mail import *
+from django.utils.translation import gettext_lazy as _
 
 class ContactSerializer(serializers.Serializer):
     SERVICE_CHOICE = [
-        ('Select a service'),
-        ('Packing and Unpacking Services', 'packing and unpacking services'),
-        ('Downsizing Services', 'downsizing services'),
-        ('Corporate and Local Relocations', 'corporate and local relocations')
+        ('', 'Select a service'),  # Empty string ensures it's not a valid choice
+        ('packing_and_unpacking', 'Packing and Unpacking Services'),
+        ('downsizing', 'Downsizing Services'),
+        ('corporate_relocations', 'Corporate and Local Relocations'),
     ]
+    
     fullName = serializers.CharField(max_length=200, write_only=True)
     phoneNumber = PhoneNumberField(region='US', write_only=True)
     email = serializers.EmailField(max_length=250, write_only=True)
     service = serializers.ChoiceField(choices=SERVICE_CHOICE, write_only=True)
-    date = serializers.DateField()
-    time = serializers.TimeField() 
+    date = serializers.DateField(write_only=True)
+    time = serializers.TimeField(write_only=True)
     address = serializers.CharField(max_length=100, write_only=True)
     message = serializers.CharField(max_length=800, write_only=True)
 
@@ -23,20 +23,22 @@ class ContactSerializer(serializers.Serializer):
         phoneNumber = data.get('phoneNumber')
         email = data.get('email')
         service = data.get('service')
-        
-        if phoneNumber.country_code != 1:
-            raise serializers.ValidationError('Phone number must be US')
-        
-        allowed_domain = ['gmail.com', 'yahoo.com', 'outlook.com']
 
-        email_domain = email.split('@') [-1]
+        # Validate Phone Number (Must be US)
+        if phoneNumber and phoneNumber.country_code != 1:
+            raise serializers.ValidationError({'phoneNumber': 'Phone number must be US'})
 
-        if email_domain not in allowed_domain:
-            raise serializers.ValidationError('Invalid email address and its not accepted')
-        
-        if service == 'Select a service':
-            raise serializers.ValidationError('Invalid service type')
-                
+        # Validate Email Domain
+        allowed_domains = ['gmail.com', 'yahoo.com', 'outlook.com']
+        email_domain = email.split('@')[-1]
+
+        if email_domain not in allowed_domains:
+            raise serializers.ValidationError({'email': 'Invalid email domain, only Gmail, Yahoo, and Outlook are accepted'})
+
+        # Validate Service Choice
+        if service == '':
+            raise serializers.ValidationError({'service': 'Invalid service type'})
+
         return data
     
     def create(self, validated_data):
